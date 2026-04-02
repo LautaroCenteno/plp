6e0106456b8c19ddde6907677f81267f6df95b91
LautaroCenteno <63695559+LautaroCenteno@users.noreply.github.com>
2026-04-01 23:42:23 -0300

    update

---
+++ guia1.hs
@@ -0,0 +1,37 @@
+--import GHC.Generics (FixityI(InfixI))
+--ejercicio 1
+
+--I
+
+--max2 :: (Float, Float) -> Float
+--normaVectorial :: (Float, Float) -> Float
+--subtract :: Float -> Float -> Float
+--predecesor :: Float -> Float
+--evaluarEnCero CONSULTAR
+--dosVeces CONSULTAR
+--flipAll :: [(a -> b -> c) -> b -> a] CONSULTAR
+--flipRaro :: a -> b -> c CONSULTAR
+
+--II
+
+--No, max2 :: Float -> Float -> Float
+--No, normaVectorial :: Float -> Float -> Float
+--Si
+--Si
+--
+--
+--
+
+--Ejercicio 2
+
+--I
+
+--curry :: ((a,b) -> c) -> (a -> b -> c)
+
+--II
+
+--uncurry :: (a -> b -> c) -> ((a,b) -> c)
+
+--III
+
+--curryN :: ((a1, ..., an) -> b) -> (a1 -> .. -> an -> b)
