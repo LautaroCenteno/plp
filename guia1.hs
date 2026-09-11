d7328a7847aecf2762cfef8874e883cf4cbb70f5
LautaroCenteno <lautarocenteno.lc@gmail.com>
2026-09-11 20:44:46 -0300

    ejercicio 8 fin

--- guia1.hs
+++ guia1.hs
@@ -1,95 +1,80 @@
---import GHC.Generics (FixityI(InfixI))
---ejercicio 1
 
---I
+--ejercicio2
 
---max2 :: (Float, Float) -> Float
---normaVectorial :: (Float, Float) -> Float
---subtract :: Float -> Float -> Float
---predecesor :: Float -> Float
---evaluarEnCero CONSULTAR
---dosVeces CONSULTAR
---flipAll :: [(a -> b -> c) -> b -> a] CONSULTAR
---flipRaro :: a -> b -> c CONSULTAR
+curry :: ((a,b) -> c) -> (a -> b -> c)
+curry f = \x y -> f(x,y)
 
---II
+uncurry :: (a -> b -> c) -> ((a, b) -> c)
+uncurry f = \(x,y) -> f x y
 
---No, max2 :: Float -> Float -> Float
---No, normaVectorial :: Float -> Float -> Float
---Si
---Si
---
---
---
 
---Ejercicio 2
-
---I
-
---curry :: ((a,b) -> c) -> (a -> b -> c)
---curry f x y = f (x, y)
-
---II
-
---uncurry :: (a -> b -> c) -> ((a,b) -> c)
---uncurry f (x, y) = f x y
-
---III
-
---curryN :: ((a1, ..., an) -> b) -> (a1 -> .. -> an -> b)
 
 --ejercicio 3
 
---I
-
-sum :: Num a => [a] -> a
-sum = foldr (+) 0
-
---elem :: Eq a => a -> [a] -> Bool
---elem | 
-
-masmas ::  [a] -> [a] -> [a] -- (++)
-masmas xs ys = foldr (:) (ys) xs
-
-filter :: (a -> Bool) -> [a] -> [a]
-filter f xs = foldr (\ x acc -> if f x then x:acc else acc) [] xs    --NO SE ME OCURRIO A MI, bastante rebuscado. No encontré otra solución
-
-map2 :: (a -> b) -> [a] -> [b]
-map2 f xs = foldr (\x acc -> (f x):acc) [] xs  --use la logica del anterior para resolverlo
-
-
---II
-mejorSegun :: (a -> a -> Bool) -> [a] -> a
-mejorSegun f xs = foldr1 (\a b -> if f a b then a else b) xs
-
---III
-sumaLista:: Num a => [a] -> a
-sumaLista [] = 0
-sumaLista (x:xs) = x + sumaLista xs
-
---sumasParciales :: Num a => [a] -> [a]
---sumasParciales 
-
-
-
---IV
-
---V
-
 --ejercicio 4
-
---I
---permutaciones:: [a] -> [[a]]
---permutaciones [] = []
---permutaciones (x:xs) = 
---
 
 --ejercicio 5
 
---elementosEnPosicionesPares NO es estructural, es global
---entrelazar es recursion estructural.
---entrelazar:: [a] -> [a]
---entrelazar [] = id
---entrelazar (x:xs) ys = if null ys
---                          then 
+--ejercicio 6
 
+--ejercicio 7
+
+--ejercicio 8
+--I
+menosCinco :: [String] -> [String]
+menosCinco [] = []
+menosCinco xs = filter ((<5) . length) xs
+
+listaAprobados :: Integral a => [a] -> [Bool]
+listaAprobados [] = []
+listaAprobados xs = map (>= 6) xs
+
+paresCuadrados :: Integral a => [a] -> [a]
+paresCuadrados [] = []
+paresCuadrados xs = map (^2) (filter ((== 0) . mod 2) xs)
+--II
+sumFold :: Num a => [a] -> a
+sumFold = foldr (+) 0
+
+elemFold :: Eq a => a -> [a] -> Bool
+elemFold e = foldr (\x rec -> x == e || rec) (False)
+
+masmasFold :: [a] -> [a] -> [a]
+masmasFold xs ys = foldr (:) ys xs
+
+filterFold :: (a -> Bool) -> [a] -> [a]
+filterFold f = foldr (\x rec -> if f x then x : rec else rec) []
+
+mapFold :: (a -> b) -> [a] -> [b]
+mapFold f = foldr (\x rec -> f x : rec) []
+
+--III
+mejorSegun :: (a -> a -> Bool) -> [a] -> a
+mejorSegun f = foldr1 (\x y -> if f x y then x else y)
+
+--IV
+sumasParciales :: Num a => [a] -> [a]
+sumasParciales [] = []
+sumasParciales (x:xs) = reverse (foldl (\e y -> (y + head e) : e) [x] xs)
+
+--V
+sumaAlt:: Num a => [a] -> a
+sumaAlt = foldr (-) 0
+
+--VI
+sumaAlt2:: Num a => [a] -> a
+sumaAlt2 = foldl (flip (-)) 0
+
+--VII
+componerTodas :: [a -> a] -> (a -> a)
+componerTodas = foldr (.) id
+
+--Ejercicio 10
+--a
+--sacarUna :: Eq a => a -> [a] -> [a]
+--sacarUna e = recr (\x xs rec -> if x == e then xs else x : rec) []
+--b
+--RESOLVER
+--c
+--instertarOrdenado :: Ord a => a -> [a] -> [a]
+--insertarOrdenado e = recr (\x xs rec -> if e <= x then e : x : xs else) []
